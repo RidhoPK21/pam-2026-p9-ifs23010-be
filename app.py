b483c2881249ab5af74ee3f17fc68c5adfe5c07c
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2026-04-23 18:22:43 +0700

    first commit

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ from app.config import Config
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=Config.APP_PORT)
+    # Tambahkan host="0.0.0.0" agar server Nginx kampus bisa mengaksesnya
+    # Ubah debug=False untuk keamanan server (Production mode)
+    app.run(host="0.0.0.0", port=int(Config.APP_PORT), debug=False)
