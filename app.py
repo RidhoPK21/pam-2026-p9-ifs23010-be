3107c2564a71c7098e5384c32204ee8b20206026
RidhoPK21 <142557165+RidhoPK21@users.noreply.github.com>
2026-04-23 19:40:57 +0700

    first commit

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from app.config import Config
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
+
 
 if __name__ == "__main__":
     # Tambahkan host="0.0.0.0" agar server Nginx kampus bisa mengaksesnya
